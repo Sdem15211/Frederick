4fe1df93f70ea2dcd34df2431209df207cb26a1a
Simon Demeulemeester <simondemeulemeester@MacBook-Pro.local>
2024-08-26 14:13:39 +0200

    circles

--- sections/Landing.tsx
+++ sections/Landing.tsx
@@ -131,6 +131,8 @@ export const Landing = () => {
           </div>
         </div>
       </Container>
+      {/* <div className="-z-10 absolute bg-circles/30 size-[46.875rem] rounded-full blur-[9rem] -left-[15.625rem] -bottom-1/2" />
+      <div className="-z-10 absolute bg-circles/30 size-[46.875rem] rounded-full blur-[9rem] -right-[17.625rem] -top-1/2" /> */}
     </section>
   );
 };
